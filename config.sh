3e3f50bc17e4901900fd2c241047e076888ee32f
William J. Bowman <wjb@williamjbowman.com>
2014-06-10 20:22:36 +0200

    Fixed sed scripts, commands, log filter
    * Sed scripts had missing \
    * Fixed commands based on experimentation
    * Fixed log filter to capture number of players

--- config.sh
+++ config.sh
@@ -6,16 +6,16 @@ SERVER_PROPERTIES=
 SERVER_PROPERTIES=${SERVER_PROPERTIES:-/srv/tekkit/server.properties}
 
 QUERY_PORT=
-QUERY_PORT=${QUERY_PORT:-(sed -n 's/^query.port=([0-9]*)$/\1/p' ${SERVER_PROPERTIES})}
+QUERY_PORT=${QUERY_PORT:-(sed -n 's/^query.port=\([0-9]*\)$/\1/p' ${SERVER_PROPERTIES})}
 
 PUBLIC_IP=
 PUBLIC_IP=${PUBLIC_IP:-`curl ifconfig.me`}
 
 LOCAL_PORT=
-LOCAL_PORT=${LOCAL_PORT:-(sed -n 's/^server-port=([0-9]*)$/\1/p' ${SERVER_PROPERTIES})}
+LOCAL_PORT=${LOCAL_PORT:-(sed -n 's/^server-port=\([0-9]*\)$/\1/p' ${SERVER_PROPERTIES})}
 
 LOCAL_IP=
-LOCAL_IP=${LOCAL_IP:-(sed -n 's/^server-ip=([0-9]*)$/\1/p' ${SERVER_PROPERTIES})}
+LOCAL_IP=${LOCAL_IP:-(sed -n 's/^server-ip=\([0-9]*\)$/\1/p' ${SERVER_PROPERTIES})}
 
 MINECRAFT_JAR=
 MINECRAFT_JAR=${MINECRAFT_PATH:-/srv/tekkit/Tekkit.jar}
@@ -49,6 +49,10 @@ START_LOCKFILE=${START_LOCKFILE:-/tmp/startingtekkit}
 IDLE_LOCKFILE=
 IDLE_LOCKFILE=${START_LOCKFILE:-/tmp/idleingtekkit}
 
+PLAYERS_FILE=
+PLAYERS_FILE=${PLAYERS_FILE:-/tmp/tekkitplayers}
+
+
 ## You may not need to change this.
 
 ## Define this function to start the minecraft server. This should start
@@ -57,9 +61,10 @@ IDLE_LOCKFILE=${START_LOCKFILE:-/tmp/idleingtekkit}
 ## This command will be run in a screen session to communicate with the
 ## server
 start() {
-  /usr/bin/java $JAVAOPTS -jar $JAVAOPTS nogui 2>&1 | grep -v -e "^INFO" -e "Can't keep up"
+  /usr/bin/java $JAVAOPTS -jar $JAVAOPTS nogui 2>&1\
+    | sed 's/\[INFO\].* There are \([0-9]*\)/[0-9]* players/\1/p' > $PLAYERS_FILE\
+    | grep -v -e "INFO" -e "Can't keep up"
 }
-
 
 ## You may not need to change this.
 
@@ -68,13 +73,12 @@ start() {
 
 ## This command will be run by crontab to stop the server.
 stop() {
-  screen -S $SESSION -p 0 -X stuff 'stop15'
+  screen -S $SESSION -p 0 -X stuff 'stop\r'
 }
 
 ## Define this function to return true if and only if the server has no
 ## players online. The server will shut down
 idle(){
-  screen -S $SESSION -p 0 -X stuff 'who15'
-  PLAYERS=echo $(tail -n 1 $MINECRAFT_LOG | cut -f 6- -d ' ' | wc -m)
-  [ "0" = "$PLAYERS" ]
+  screen -S $SESSION -p 0 -X stuff 'list\r'
+  [ "0" = `cat $PLAYERS_FILE` ]
 }
