4dab3d27bccfd93323c8a1a93ffa1338c6faa692
William J. Bowman <wjb@williamjbowman.com>
2014-06-11 11:14:26 -0400

    Tons of fixing after rigorous testing
    * Added debugging messages/support
    * Fixed parsing of players online; lots of extra control characters seem
      to be getting into the file. The current fix it to remove them
      (instead of preventing them from being printed in the first place)
    * Reworked start script so sign posting always shows up/prevent
      lockfile from being removed until the server is responding to pings.

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ tekkit-on-demand
 
 A set of script for an on-demand Tekkit server.
 
+Requires netcat, lsof, and sh.
+
 You should see my blog about this project: TODO
--- config.sh
+++ config.sh
@@ -28,6 +28,8 @@ JAVAOPTS=${JAVAOPTS:--Xmx2G -Xms1G -server -XX:+UseG1GC -XX:MaxGCPauseMillis=50 
 SESSION=
 SESSION=${SESSION:-Minecraft}
 
+## TODO: Currenently not used. Need to recompute size and UTF-16BE
+## encode the message, which is annoying
 MESSAGE=
 MESSAGE=${MESSAGE:-Just a moment please}
 
@@ -49,6 +51,10 @@ IDLE_LOCKFILE=${START_LOCKFILE:-/tmp/idleingtekkit}
 PLAYERS_FILE=
 PLAYERS_FILE=${PLAYERS_FILE:-/tmp/tekkitplayers}
 
+debug() {
+  #echo "$1"
+  echo -n ""
+}
 ## You may not need to change this.
 
 ## Define this function to start the minecraft server. This should start
@@ -58,7 +64,7 @@ PLAYERS_FILE=${PLAYERS_FILE:-/tmp/tekkitplayers}
 ## server
 start() {
   /usr/bin/java $JAVAOPTS -jar $MINECRAFT_JAR nogui 2>&1 \
-    | sed -e 's/^.*There are \([0-9]*\)\/[0-9] players.*$/\1/' -e 't M' -e 'b' -e ": M w $PLAYERS_FILE" -e 'd' \
+    | sed -n -e 's/^.*There are \([0-9]*\)\/[0-9] players.*$/\1/' -e 't M' -e 'b' -e ": M w $PLAYERS_FILE" -e 'd' \
     | grep -v -e "INFO" -e "Can't keep up"
 }
 
@@ -69,13 +75,25 @@ start() {
 
 ## This command will be run by crontab to stop the server.
 stop() {
-  screen -S $SESSION -p 0 -X stuff 'stop\r'
+  screen -S $SESSION -p 0 -X stuff 'stop\15'
+  debug "Shit's going down"
 }
 
 ## Define this function to return true if and only if the server has no
 ## players online. The server will shut down
-idle(){
-  echo -n "" > $PLAYERS_FILE
-  screen -S $SESSION -p 0 -X stuff 'list\r'
-  [ "0" = `tail -n 1 $PLAYERS_FILE` ]
+idle() {
+  screen -S $SESSION -p 0 -X stuff 'list\15'
+  while ! lsof | grep $PLAYERS_FILE; do
+    sleep 1
+  done
+  cat $PLAYERS_FILE | tr -d [:cntrl:] > $PLAYERS_FILE
+  players=`cat $PLAYERS_FILE`
+  debug "There are $players players"
+  if [ "0" = "$players" ]; then
+    debug "Idle"
+    true
+  else
+    debug "Not idle"
+    false
+  fi
 }
